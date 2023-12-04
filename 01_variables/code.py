d864890400022948ef5df9b44b93cf34f69e75ea
Arundas MK <codewitharun.com@gmail.com>
2023-12-04 21:10:02 +0530

    initial commit

---
+++ 01_variables/code.py
@@ -0,0 +1,36 @@
+# -- Numbers and Floats --
+
+x = 10
+price = 5.27
+
+discount = 0.5
+
+result = price * (1 - discount)
+
+print(result)
+
+# -- Strings --
+
+name = "Arun"
+name = "Arun"
+
+print(name)
+print(name * 5)
+
+# -- Changing variables --
+# Variables are names for values.
+
+a = 30
+b = a
+
+# Here we've given the value '30' the names 'a' and 'b'.
+
+print(a)
+print(b)
+
+b = 17
+
+# Here we've given the value '17' the name 'b'. The name 'a' is still a name for '25'!
+
+print(a)
+print(b)
